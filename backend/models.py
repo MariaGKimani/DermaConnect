eeeccc3116e1b8ec492cdd4cb98e58b3f9913b0c
Mariam Senzia <mariamsenzia@gmail.com>
2024-09-25 12:57:42 +0300

    Created a user model

--- backend/models.py
+++ backend/models.py
@@ -2,3 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String(150))
+    phone_number = db.Column(db.String(15))
+    password = db.Column(db.String)
+    message = db.Column(db.String(150))
+
+
