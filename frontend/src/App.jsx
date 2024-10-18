1dc7248aaefdc9c4a974afcdf5b4876083a0cd28
MariaGKimani <mariakimanigoretti@gmail.com>
2024-10-18 11:39:52 +0300

    Fixed alignment on the navbar.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,7 @@ function App() {
   return (
     <>
       <ChakraProvider>
-        <Navbar/>
-        <Footer />
+       
       </ChakraProvider>
     </>
   )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,19 +8,19 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
     return(
         <>
-              <Box display="flex" justifyContent="space-between" alignItems="center" p="1rem" boxShadow="md">
-              <Image src={logo} alt="DermaConnect logo" height='150px' ml='5rem'/>
-              <Spacer />
-              <Flex alignItems="center" mr='8rem' >
-                <Button color="white" bg="#0A345C"
-                _hover={{bg:"#072d49"}}
-                mr="2rem"> Are you a Dermatologist</Button>
-                <Button colorScheme="blue" variant="outline" borderColor="#0A345C" borderWidth="2px"  mr="2rem" textColor={'black'}>
-            Login
-          </Button>
-          <IconButton icon={<FontAwesomeIcon icon={faUser} />} aria-label='Contact Us' color="#00BFFF" bg="transparent" _hover={{ color: "#072d49", bg: "transparent" }} isRound  fontSize='2.0rem'/>
-              </Flex>
-              </Box>
+            <Box display="flex" justifyContent="space-between" alignItems="center" p="1rem" boxShadow="md" position="fixed" zIndex="1000">
+                <Image src={logo} alt="DermaConnect logo" height='150px' ml='5rem'/>
+                <Spacer />
+                <Flex alignItems="center" mr='8rem' >
+                    <Button color="white" bg="#0A345C" _hover={{bg:"#072d49"}} mr="2rem">
+                     Are you a Dermatologist
+                    </Button>
+                    <Button colorScheme="blue" variant="outline" borderColor="#0A345C" borderWidth="2px"  mr="2rem" textColor={'black'}>
+                     Login
+                    </Button>
+                    <IconButton icon={<FontAwesomeIcon icon={faUser} />} aria-label='Contact Us' color="#00BFFF" bg="transparent" _hover={{ color: "#072d49", bg: "transparent" }} isRound  fontSize='2.0rem'/>
+                </Flex>
+            </Box>
         
         </>
     )
