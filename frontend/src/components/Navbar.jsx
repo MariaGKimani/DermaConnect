195ed282b04395fdbe5bfd79928ccdd2db5264dd
MariaGKimani <mariakimanigoretti@gmail.com>
2024-10-22 16:47:18 +0300

    Fixes the buttons

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,11 +8,11 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
     return(
         <>
-            <Box display="flex" justifyContent="space-between" alignItems="center" p="1rem" boxShadow="md" position="fixed"  top="0" left="0" right="0" zIndex='1000' height='90px' bg={'white'}>
+            <Box display="flex" justifyContent="space-between" alignItems="center" p="1rem" boxShadow="md" position="fixed"  top="0" left="0" right="0" zIndex='1000' height='90px'  bg="transparent">
                 <Image src={logo} alt="DermaConnect logo" height='150px' ml='5rem'/>
                 <Spacer />
                 <Flex alignItems="center" mr='8rem' >
-                    <Button color="white" bg="#0A345C" _hover={{bg:"#072d49"}} mr="2rem">
+                    <Button color="white" bg="#0A345C" _hover={{bg: 'transparent',color: '#0A345C', border: '2px solid #0A345C',}} mr="2rem">
                      Are you a Dermatologist
                     </Button>
                     <Button colorScheme="blue" variant="outline" borderColor="#0A345C" borderWidth="2px"  mr="2rem" textColor={'black'}>
