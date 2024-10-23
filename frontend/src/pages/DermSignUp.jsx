b9acdef831265faf928d827d22bfef9dc1a17069
MariaGKimani <mariakimanigoretti@gmail.com>
2024-10-23 10:43:40 +0300

    replaced vstack and hstack with flex

--- frontend/src/pages/DermSignUp.jsx
+++ frontend/src/pages/DermSignUp.jsx
@@ -1,169 +1,168 @@
 import React from "react";
 import { 
-  Box, Button, FormControl,Image, FormLabel, Input, Select,Textarea, Stack, HStack, VStack, RadioGroup, Radio, Flex
+  Box, Button, FormControl, Image, FormLabel, Input, Select, Textarea, Flex, RadioGroup, Radio
 } from "@chakra-ui/react";
-import logo from "../assets/Pink_Minimal_Skincare___Hair_Free_Logo__1_-removebg-preview.png"
-
+import logo from "../assets/Pink_Minimal_Skincare___Hair_Free_Logo__1_-removebg-preview.png";
 
 const DermatologistSignupForm = () => {
   return (
     <Box p={'8'} borderWidth="1px" borderRadius="lg">
-      <Box textAlign="center" mb={5}>
-        <Image src={logo} alt='dermaconnect-logo' height='200px' ml='-5rem'/>
+      <Box textAlign="center" mb={0}>
+        <Image src={logo} alt='dermaconnect-logo' height='200px' ml='-5rem' mb={0}/>
       </Box>
       <Box as="h1" fontSize="2xl" fontWeight="bold" textAlign="center" mb={'0'} >
         CREATE AN ACCOUNT
       </Box>
 
-      <Flex justify="space-between" align="flex-start" w="100%" >
-        <VStack w="60%" spacing={4}>
-          <HStack w="100%" spacing={4}>
+      <Flex justify="space-between" align="flex-start" w="100%">
+        <Flex direction="column" w="60%" gap={4}>
+          <Flex w="100%" gap={4}>
             <FormControl isRequired>
-              <FormLabel   fontWeight="bold">First Name</FormLabel>
-              <Input placeholder="First Name" borderColor="#0A345C"  />
+              <FormLabel fontWeight="bold">First Name</FormLabel>
+              <Input placeholder="First Name" borderColor="#0A345C"  focusBorderColor="#0A345C"/>
             </FormControl>
 
             <FormControl isRequired>
               <FormLabel fontWeight="bold">Last Name</FormLabel>
-              <Input placeholder="Last Name" borderColor="#0A345C"/>
+              <Input placeholder="Last Name" borderColor="#0A345C"  focusBorderColor="#0A345C"/>
             </FormControl>
-          </HStack>
+          </Flex>
 
-          <HStack w="100%" spacing={4}>
+          <Flex w="100%" gap={4}>
             <FormControl isRequired>
               <FormLabel fontWeight="bold">Number</FormLabel>
-              <Input placeholder="(+92)..." borderColor="#0A345C" />
+              <Input placeholder="(+92)..." borderColor="#0A345C"  focusBorderColor="#0A345C" />
             </FormControl>
 
             <FormControl isRequired>
               <FormLabel fontWeight="bold">Email Address</FormLabel>
-              <Input placeholder="example@domain.com" borderColor="#0A345C" />
+              <Input placeholder="example@domain.com" borderColor="#0A345C"  focusBorderColor="#0A345C" />
             </FormControl>
-          </HStack>
-
-          <HStack w="100%" spacing={4}>
+          </Flex>
+          
+          <Flex w="100%" gap={4}>
             <FormControl isRequired>
               <FormLabel fontWeight="bold">Password</FormLabel>
-              <Input type="password" placeholder="Password" borderColor="#0A345C"/>
+              <Input type="password" placeholder="Password" borderColor="#0A345C"  focusBorderColor="#0A345C"/>
             </FormControl>
 
             <FormControl isRequired>
               <FormLabel fontWeight="bold">Confirm Password</FormLabel>
-              <Input type="password" placeholder="Confirm Password" borderColor="#0A345C" />
+              <Input type="password" placeholder="Confirm Password" borderColor="#0A345C"  focusBorderColor="#0A345C" />
             </FormControl>
-          </HStack>
+          </Flex>
 
-          <HStack w="100%" spacing={4}>
+          <Flex w="100%" gap={4}>
             <FormControl>
               <FormLabel fontWeight="bold">Country</FormLabel>
-              <Input placeholder="Country" borderColor="#0A345C" />
+              <Input placeholder="Country" borderColor="#0A345C"  focusBorderColor="#0A345C" />
             </FormControl>
 
             <FormControl>
               <FormLabel fontWeight="bold">City</FormLabel>
-              <Input placeholder="City" borderColor="#0A345C" />
+              <Input placeholder="City" borderColor="#0A345C"  focusBorderColor="#0A345C" />
             </FormControl>
-          </HStack>
+          </Flex>
 
-          <HStack w="100%" spacing={4}>
+          <Flex w="100%" gap={4}>
             <FormControl>
               <FormLabel fontWeight="bold">Medical ID</FormLabel>
-              <Input placeholder="Medical ID" borderColor="#0A345C" />
+              <Input placeholder="Medical ID" borderColor="#0A345C"  focusBorderColor="#0A345C" />
             </FormControl>
 
             <FormControl>
               <FormLabel fontWeight="bold">Level of Education</FormLabel>
-              <Select placeholder="Select education level" borderColor="#0A345C" >
+              <Select placeholder="Select education level" borderColor="#0A345C"  focusBorderColor="#0A345C" >
                 <option value="undergraduate">Undergraduate</option>
                 <option value="graduate">Graduate</option>
                 <option value="postgraduate">Postgraduate</option>
               </Select>
             </FormControl>
-          </HStack>
-        </VStack>
+          </Flex>
+        </Flex>
 
-        <VStack w="35%" spacing={4} align="flex-start">
+        <Flex direction="column" w="35%" gap={4} align="flex-start">
           <Box fontSize="lg" fontWeight="bold" mb={4}>
             Attach documents
           </Box>
-          <HStack w="100%" spacing={10}>
-            <VStack spacing={14} w="50%">
+          <Flex w="100%" gap={10}>
+            <Flex direction="column" gap={14} w="50%">
               <FormControl>
                 <FormLabel fontWeight="bold">Professional Photo</FormLabel>
-                <Input type="file" color="white"  />
+                <Input type="file" color="#48484B" />
               </FormControl>
 
               <FormControl>
                 <FormLabel fontWeight="bold">Medical License</FormLabel>
-                <Input type="file" color="white"  />
+                <Input type="file" color="#48484B" />
               </FormControl>
-            </VStack>
+            </Flex>
 
-            <VStack spacing={14} w="50%">
+            <Flex direction="column" gap={14} w="50%">
               <FormControl>
                 <FormLabel fontWeight="bold">Qualification/Degrees</FormLabel>
-                <Input type="file" color="white" />
+                <Input type="file" color="#48484B" />
               </FormControl>
 
               <FormControl>
-                <FormLabel fontWeight="bold">Upload ID/Passport</FormLabel>
-                <Input type="file"  color="white"  />
+                <FormLabel fontWeight="bold" >Upload ID/Passport</FormLabel>
+                <Input type="file" color="#48484B" />
               </FormControl>
-            </VStack>
-          </HStack>
-        </VStack>
+            </Flex>
+          </Flex>
+        </Flex>
       </Flex>
 
-      <Flex justify="space-between" align="flex-start" w="100%" spacing={10}>
-        <VStack w="60%" >
+      <Flex justify="space-between" align="flex-start" w="100%" gap={10}>
+        <Flex direction="column" w="60%">
           <FormControl>
-            <FormLabel fontWeight="bold">Describe Your Expertise and Services</FormLabel>
-            <Textarea placeholder="Describe your services"  _placeholder={{ color: "#0A345C" }} />
+            <FormLabel fontWeight="bold" mt={5} >Describe Your Expertise and Services</FormLabel>
+            <Textarea placeholder="Describe your services" borderColor="#0A345C"  focusBorderColor="#0A345C"/>
           </FormControl>
-        </VStack>
+        </Flex>
 
-        <VStack w="35%" spacing={4} align="flex-start">
-          <Flex w="100%" justify="space-between" spacing={10}>
-            <VStack spacing={4} w="50%">
-              <FormControl>
-                <FormLabel fontWeight="bold">Consultation Fees</FormLabel>
-                <Select placeholder="5000-10000">
-                  <option value="5000">5000</option>
-                  <option value="10000">10000</option>
-                </Select>
-              </FormControl>
-
+        <Flex direction="column" w="35%" gap={4} align="flex-start">
+          <Flex w="100%" justify="space-between" gap={10}>
+            <Flex direction="column" gap={4} w="50%">
               <FormControl>
                 <FormLabel fontWeight="bold">Cost per Session</FormLabel>
-                <Select placeholder="Select cost" >
-                  <option value="500">500</option>
-                  <option value="1000">1000</option>
+                <Select placeholder="Select cost" borderColor="#0A345C" focusBorderColor="#0A345C">
+                  <option value="500">5000</option>
+                  <option value="1000">10000</option>
                 </Select>
               </FormControl>
-            </VStack>
-            <Spacer />
+            </Flex>
 
-            <VStack spacing={14} w="50%">
+            <Flex direction="column" gap={14} w="50%">
               <FormControl>
                 <FormLabel fontWeight="bold">Payment Method</FormLabel>
                 <RadioGroup defaultValue="Mpesa">
-                  <Stack direction="row">
+                  <Flex direction="row" gap={4}>
                     <Radio value="Mpesa">Mpesa</Radio>
                     <Radio value="Card">Card</Radio>
-                  </Stack>
+                  </Flex>
                 </RadioGroup>
               </FormControl>
-            </VStack>
+            </Flex>
           </Flex>
-        </VStack>
+        </Flex>
       </Flex>
 
-      <Box mt={8}>
-            <Button bg={'#0A345C'} color="white" size='lg' _hover={{bg: 'transparent',color: '#0A345C', border: '2px solid #0A345C',}}>
-            Signup
-            </Button>
-       </Box>
-
+      <Box mt={8} display="flex" justifyContent="center">
+        <Button 
+          bg={'#0A345C'} 
+          color="white" 
+          size='lg' 
+          px={28}
+          _hover={{
+            bg: 'transparent',
+            color: '#0A345C', 
+            border: '2px solid #0A345C',
+          }}
+        >
+          Signup
+        </Button>
+      </Box>
     </Box>
   );
 };
